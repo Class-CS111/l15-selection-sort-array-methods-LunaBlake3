dd748df902786d78abad3c0bd85ee8c5b44bbeda
LunaBlake3 <lunablaker@gmail.com>
2025-04-04 22:15:46 +0000

    project complete

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,8 +1,8 @@
 /********************************************
-*	AUTHOR:	<name>
+*	AUTHOR:	Blake Luna-Beltran
 * COLLABORATORS: <names>
 *	COURSE:	CS 111 Intro to CS I - Java
-*	LAST MODIFIED: <date>
+*	LAST MODIFIED: 04/04/2025
 ********************************************/
 
 /********************************************
@@ -11,8 +11,7 @@
 *	PROGRAM DESCRIPTION:
 *	Collection of useful methods for int arrays.
 *********************************************
-*	ALGORITHM:
-*	TODO: <Pseudocode for selection sort here>
+
 *********************************************
 
 /* UML CLASS DIAGRAM:
@@ -33,31 +32,59 @@ public class ArrayMethods
   /**DESCRIPTION: */
   public static String arrayString(int[] a)
   {
-    return ""; //STUB to keep compiler happy
+    String result = "{ " + a[0];
+    for(int i=1; i<a.length; i++)
+    {
+      result += ", " + a[i] ;
+    }
+    result += " }";
+
+    return result;
   }
   
   /**DESCRIPTION: */
   public static void swap(int[] array, int a, int b)
   {
-  
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
   }
 	
 	/**DESCRIPTION: */
   public static int indexOfMin(int[] array, int startIndex)
   {
-    return 0; //STUB to keep compiler happy
+    int minValue = array[startIndex];
+    int minIndex = startIndex;
+
+    for(int i=startIndex+1; i<array.length; i++)
+    {
+      if(minValue > array[i])
+      {
+        minValue = array[1];
+        minIndex = i;
+      }
+    }
+
+    return minIndex;
   }
 
 	/**DESCRIPTION: */
   public static void reverse(int[] array)
   {
-
+    for(int i=0; i<array.length/2; i++)
+    {
+      ArrayMethods.swap(array, i, array.length-1-i);
+    }
   }
 
 	/**DESCRIPTION: */
   public static void selectionSort(int[] array)
   {
-
+    for(int i = 0; i < array.length-1; i++)
+    {
+    int indexMinValue = ArrayMethods.indexOfMin(array, i);
+    ArrayMethods.swap(array, i, indexMinValue);
+    }
   }
 
 }
